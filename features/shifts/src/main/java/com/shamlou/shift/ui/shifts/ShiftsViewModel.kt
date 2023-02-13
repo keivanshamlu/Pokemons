ec4ae7a56173836ef3ffede1a7b71b4d3a728ffc
Keivan Shamlou <keivan.shamlou@gamesys.co.uk>
2023-02-13 16:28:17 +0000

    new ways

--- features/shifts/src/main/java/com/shamlou/shift/ui/shifts/ShiftsViewModel.kt
+++ features/shifts/src/main/java/com/shamlou/shift/ui/shifts/ShiftsViewModel.kt
@@ -1,5 +1,6 @@
 package com.shamlou.shift.ui.shifts
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.shamlou.bases.mapper.Mapper
@@ -16,13 +17,16 @@ import com.shamlou.shift.model.shifts.ResponseShiftDataView
 import com.shamlou.shift.model.shifts.ResponseShiftsView
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class ShiftsViewModel @AssistedInject constructor(@Assisted private val savedStateHandle: SavedStateHandle,
-                                                  private val getShiftsUseCase : FlowUseCase<Unit, ResponseShiftsDomain>,
-                                                  private val mapperShiftsDomainToView : Mapper<ResponseShiftsDomain, ResponseShiftsView>,
-                                                  private val mapperShiftsViewToNavModel : Mapper<ResponseShiftDataView, NavModelMap>,
+class ShiftsViewModel @AssistedInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle,
+    private val getShiftsUseCase: FlowUseCase<Unit, ResponseShiftsDomain>,
+    private val mapperShiftsDomainToView: Mapper<ResponseShiftsDomain, ResponseShiftsView>,
+    private val mapperShiftsViewToNavModel: Mapper<ResponseShiftDataView, NavModelMap>,
 ) : BaseViewModel() {
 
     //holds shifts and their status
@@ -42,44 +46,93 @@ class ShiftsViewModel @AssistedInject constructor(@Assisted private val savedSta
 
     init {
 
-        getShiftsAsync()
+        async()
+        asyncAnotherWay()
+
+        chained()
+        chainedAnotherWay()
     }
 
-    /**
-     * gets all items at the beginning and maps it to view objects
-     * delegates error handling to base [handleNetworkError] in [BaseViewModel]
-     */
-    fun getShiftsAsync() {
+    fun async() = viewModelScope.launch {
+        val a = async {
+            request1()
+        }
+        val b = async {
+            request2()
+        }
+        awaitAll(a, b)
+    }
+
+    fun asyncAnotherWay() {
         viewModelScope.launch {
-            _shifts.emit(Resource.loading())
-            getShiftsUseCase.invoke(Unit).map {
-                it.map(mapperShiftsDomainToView)
-            }.collect {
-                handleNetworkError(it)
-                _shifts.emit(it)
-            }
+            request1()
+        }
+        viewModelScope.launch {
+            request2()
         }
     }
 
+    fun chained() = viewModelScope.launch {
+        launch {
+            request1()
+            request2()
+        }
+    }
+
+    fun chainedAnotherWay() = viewModelScope.launch {
+        val a = launch {
+            request1()
+        }
+        a.join()
+        val b = launch {
+            request2()
+        }
+        b.join()
+    }
+
+    private suspend fun request1(){
+        _shifts.emit(Resource.loading())
+        Log.d("TESTEST", "start request 1")
+        getShiftsUseCase.invoke(Unit).map {
+            it.map(mapperShiftsDomainToView)
+        }.collect {
+            handleNetworkError(it)
+            _shifts.emit(it)
+            Log.d("TESTEST", "response 1")
+        }
+    }
+    private suspend fun request2(){
+        _shifts.emit(Resource.loading())
+        Log.d("TESTEST", "start request 2")
+        getShiftsUseCase.invoke(Unit).map {
+            it.map(mapperShiftsDomainToView)
+        }.collect {
+            handleNetworkError(it)
+            _shifts.emit(it)
+            Log.d("TESTEST", "response 2")
+        }
+    }
     /**
      * calls when user click on login button
      */
-    fun loginClicked(){
+    fun loginClicked() {
 
         _toLogin.tryEmit(Event(Unit))
     }
+
     /**
      * calls when user click on sign up button
      */
-    fun signUpClicked(){
+    fun signUpClicked() {
 
         _toSignUp.tryEmit(Event(Unit))
     }
+
     /**
      * calls when user click on a shift
      */
-    fun itemClicked(clickedItem :ResponseShiftDataView){
-        
+    fun itemClicked(clickedItem: ResponseShiftDataView) {
+
         mapperShiftsViewToNavModel.map(clickedItem).also {
 
             navigateTo(NavigationFlow.ToMap(it))
